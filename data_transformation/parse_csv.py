97eccd42cc66f826c5744bde73cc87de3d28db54
Alexandra Dobkin <adobkin7@c02qv2bpg8wm.gateway.sonic.net>
2017-01-16 16:07:15 -0800

    about to start on mobile

--- data_transformation/parse_csv.py
+++ data_transformation/parse_csv.py
@@ -41,7 +41,6 @@ def get_unqiue_row_data_from_specified_headers(school_objects_list):
 		grades_served.update(school.grades_served)
 		start_time.update(school.start_time)
 		end_time.update(school.end_time)
-
 		neighborhood.update(school.neighborhood)
 		multilingual_pathways.update(school.multilingual_pathways)
 		before_school_program.update(school.before_school_program)
@@ -49,6 +48,16 @@ def get_unqiue_row_data_from_specified_headers(school_objects_list):
 		after_school_program.update(school.after_school_program)
 		after_school_program_offerings.update(school.after_school_program_offerings)
 
+	# remove blank entries
+	grades_served.discard("")
+	start_time.discard("")
+	end_time.discard("")
+	neighborhood.discard("")
+	multilingual_pathways.discard("")
+	before_school_program.discard("")
+	before_school_program_offerings.discard("")
+	after_school_program.discard("")
+	after_school_program_offerings.discard("")
 	
 	unique_content = {"grades_served": sorted(grades_served), 
 					  "start_time": sorted(start_time), 
@@ -66,13 +75,13 @@ def get_matching_schools(school_objects_list, matching_parameters):
 	output_schools = []
 	for school in school_objects_list:
 		print "matching_parameters: ", matching_parameters
-		if ([neighborhood for neighborhood in matching_parameters["neighborhood"] if neighborhood in school.neighborhood] 
-			and [grades_served for grades_served in matching_parameters["grades_served"] if grades_served in school.grades_served] 
-			and [before_school_program for before_school_program in matching_parameters["before_school_program"] if before_school_program in school.before_school_program]
-			and [before_school_program_offerings for before_school_program_offerings in matching_parameters["before_school_program_offerings"] if before_school_program_offerings in school.before_school_program_offerings]
-			and [multilingual_pathways for multilingual_pathways in matching_parameters["multilingual_pathways"] if multilingual_pathways in school.multilingual_pathways]  
-			and [after_school_program for after_school_program in matching_parameters["after_school_program"] if after_school_program in school.after_school_program]
-			and [after_school_program_offerings for after_school_program_offerings in matching_parameters["after_school_program_offerings"] if after_school_program_offerings in school.after_school_program_offerings]):  
+		if ([neighborhood for neighborhood in matching_parameters["neighborhood"] if neighborhood in school.neighborhood or "" in school.neighborhood] 
+			and [grades_served for grades_served in matching_parameters["grades_served"] if grades_served in school.grades_served or "" in school.grades_served] 
+			and [before_school_program for before_school_program in matching_parameters["before_school_program"] if before_school_program in school.before_school_program or "" in school.before_school_program]
+			and [before_school_program_offerings for before_school_program_offerings in matching_parameters["before_school_program_offerings"] if before_school_program_offerings in school.before_school_program_offerings  or "" in school.before_school_program_offerings]
+			and [multilingual_pathways for multilingual_pathways in matching_parameters["multilingual_pathways"] if multilingual_pathways in school.multilingual_pathways or "" in school.multilingual_pathways]  
+			and [after_school_program for after_school_program in matching_parameters["after_school_program"] if after_school_program in school.after_school_program or "" in school.after_school_program]
+			and [after_school_program_offerings for after_school_program_offerings in matching_parameters["after_school_program_offerings"] if after_school_program_offerings in school.after_school_program_offerings or "" in school.after_school_program_offerings]):  
 		   	output_schools.append({"name": school.name, 
 		   						   "start_time": school.start_time,
 		   						   "end_time": school.end_time,
